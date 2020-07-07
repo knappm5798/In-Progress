7abc86c1c02e9fd2766fb988ab09898ed0f59303
knappm5798 <67263929+knappm5798@users.noreply.github.com>
2020-07-07 04:09:49 +0300

    Add files via upload

---
+++ P4HW1_Expenses_MichaelKnapp.py
@@ -0,0 +1,15 @@
+# CTI-110
+# P4HW1 - Expenses
+# Michael Knapp
+# 20200704
+#
+expense= float(input())
+account= float(input())
+
+try:
+    print('What is the starting amount in the account?' + 'Please enter postive number:'
+    if account >= 0:
+        break
+    except(St)
+
+        
